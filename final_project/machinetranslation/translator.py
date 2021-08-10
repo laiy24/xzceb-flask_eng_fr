c4c093bd8b6bd6fadad19e270178cc63ea56a274
laiy24 <71063139+laiy24@users.noreply.github.com>
2021-08-10 10:50:17 +0800

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -4,6 +4,8 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
+
+
 load_dotenv()
 
 apikey = os.environ['apikey']
